c7378a289f4f577e4811f6ec22a0e8301b4942fc
True Leifee!!! <102321360+neeLeifee@users.noreply.github.com>
2025-03-01 01:07:32 +0300

    Piece class functioning!

--- FA_chess.py
+++ FA_chess.py
@@ -6,76 +6,102 @@ class Table():
 
     def getSize(self):return(self.size)
 
+class Piece():
+    def __init__(self, color, type, position):
+        self.color = color
+        self.type = type
+        self.position = position
+        
+        pieces_skins = {
+            'wP' : '♙',  # белая пешка
+            'wN' : '♘',  # белый конь
+            'wB' : '♗',  # белый слон
+            'wR' : '♖',  # белая ладья
+            'wQ' : '♕',  # белый ферзь
+            'wK' : '♔',  # белый король
+
+            'bP' : '♟',  # чёрная пешка
+            'bN' : '♞',  # чёрный конь
+            'bB' : '♝',  # чёрный слон
+            'bR' : '♜',  # чёрная ладья
+            'bQ' : '♛',  # чёрный ферзь
+            'bK' : '♚',  # чёрный король
+        }
+
+        self.skin = pieces_skins[self.color + self.type]
+    
+    def getPosition(self):
+        return self.position
+    def setPosition(self, position):
+        self.position = position
+
+    def getSkin(self):
+        return self.skin
+
 class Game():
     def __init__(self):
         self.table = Table([8,8])
 
-        self.pieces_list = {
-            'wP' : '♙',  # пешка
-            'wN' : '♘',  # конь
-            'wB' : '♗',  # слон
-            'wR' : '♖',  # ладья
-            'wQ' : '♕',  # ферзь
-            'wK' : '♔',  # король
-
-            'bP' : '♟',  # пешка
-            'bN' : '♞',  # конь
-            'bB' : '♝',  # слон
-            'bR' : '♜',  # ладья
-            'bQ' : '♛',  # ферзь
-            'bK' : '♚',  # король
-        }
-        self.white_positions = [
-            'wP1' , 'A2',  # пешка
-            'wP2' , 'B2',  # пешка
-            'wP3' , 'C2',  # пешка
-            'wP4' , 'D2',  # пешка
-            'wP5' , 'E2',  # пешка
-            'wP6' , 'F2',  # пешка
-            'wP7' , 'G2',  # пешка
-            'wP8' , 'H2',  # пешка
-
-            'wN1' , 'B1',  # конь
-            'wN2' , 'F1',  # конь
-
-            'wB1' , 'C1',  # слон
-            'wB2' , 'G1',  # слон
-
-            'wR1' , 'A1',  # ладья
-            'wR2' , 'H1',  # ладья
-
-            'wQ1' , 'D1',  # ферзь
-            'wK1' , 'E1',  # король
-        ]
-        self.black_positions = [
-            'bP1' , 'A7',  # пешка
-            'bP2' , 'B7',  # пешка
-            'bP3' , 'C7',  # пешка
-            'bP4' , 'D7',  # пешка
-            'bP5' , 'E7',  # пешка
-            'bP6' , 'F7',  # пешка
-            'bP7' , 'G7',  # пешка
-            'bP8' , 'H7',  # пешка
-
-            'bN1' , 'B8',  # конь
-            'bN2' , 'F8',  # конь
-
-            'bB1' , 'C8',  # слон
-            'bB2' , 'G8',  # слон
-
-            'bR1' , 'A8',  # ладья
-            'bR2' , 'H8',  # ладья
-
-            'bQ1' , 'D8',  # ферзь
-            'bK1' , 'E8',  # король
-        ]
-
         self.white_score = 0
         self.black_score = 0
 
+        self.setPieces()
+
+    def setPieces(self):
+        # Документация наименования фигур
+        # w - White, b - Black
+        # P - пешка, 
+        # N - конь
+        # B - слон
+        # R - ладья
+        # Q - ферзь
+        # K - король
+        self.white_figures = [
+            Piece('w', 'P', 'A2'),  # пешка
+            Piece('w', 'P', 'B2'),  # пешка
+            Piece('w', 'P', 'C2'),  # пешка
+            Piece('w', 'P', 'D2'),  # пешка
+            Piece('w', 'P', 'E2'),  # пешка
+            Piece('w', 'P', 'F2'),  # пешка
+            Piece('w', 'P', 'G2'),  # пешка
+            Piece('w', 'P', 'H2'),  # пешка
+
+            Piece('w', 'N', 'B1'),  # конь
+            Piece('w', 'N', 'F1'),  # конь
+
+            Piece('w', 'B', 'C1'),  # слон
+            Piece('w', 'B', 'G1'),  # слон
+
+            Piece('w', 'R', 'A1'),  # ладья
+            Piece('w', 'R', 'H1'),  # ладья
+
+            Piece('w', 'Q', 'D1'),  # ферзь
+            Piece('w', 'K', 'E1'),  # король
+        ]
+        self.black_figures = [
+            Piece('b', 'P', 'A7'),  # пешка
+            Piece('b', 'P', 'B7'),  # пешка
+            Piece('b', 'P', 'C7'),  # пешка
+            Piece('b', 'P', 'D7'),  # пешка
+            Piece('b', 'P', 'E7'),  # пешка
+            Piece('b', 'P', 'F7'),  # пешка
+            Piece('b', 'P', 'G7'),  # пешка
+            Piece('b', 'P', 'H7'),  # пешка
+
+            Piece('b', 'N', 'B8'),  # конь
+            Piece('b', 'N', 'F8'),  # конь
+
+            Piece('b', 'B', 'C8'),  # слон
+            Piece('b', 'B', 'G8'),  # слон
+
+            Piece('b', 'R', 'A8'),  # ладья
+            Piece('b', 'R', 'H8'),  # ладья
+
+            Piece('b', 'Q', 'D8'),  # ферзь
+            Piece('b', 'K', 'E8'),  # король
+        ]
 
     def printField(self):
-
         #drawing first line of letters
         print("   ",
               end='')
@@ -83,6 +109,15 @@ class Game():
         print('\n','\n',
               sep='', end='')
 
+        white_positions = []
+        for figure in self.white_figures:
+            white_positions.append(figure.getSkin())
+            white_positions.append(figure.getPosition())
+
+        black_positions = []
+        for figure in self.black_figures:
+            black_positions.append(figure.getSkin())
+            black_positions.append(figure.getPosition())
 
         #drawing numbers(i loop) and figures(j loop)
         #i - номер строки
@@ -96,17 +131,17 @@ class Game():
                 tile_num = alph[j]+str(i+1)
 
                 #printing piece
-                if tile_num in self.white_positions:    #white pieces
-                    current_piece_name = self.white_positions[self.white_positions.index(tile_num)-1]
-                    print(self.pieces_list[current_piece_name[:2]], ' ',
+                if tile_num in white_positions:    #white pieces
+                    current_piece = white_positions[white_positions.index(tile_num)-1]
+                    print(current_piece, ' ',
                         sep='',end='')
-                elif tile_num in self.black_positions:  #black pieces
-                    current_piece_name = self.black_positions[self.black_positions.index(tile_num)-1]
-                    print(self.pieces_list[current_piece_name[:2]], ' ',
+                elif tile_num in black_positions:  #black pieces
+                    current_piece = black_positions[black_positions.index(tile_num)-1]
+                    print(current_piece, ' ',
                         sep='',end='')
-                else:                                   #filling whats left with dots
-                    print('. ',sep='',end='')
-                    #print(tile_num,sep='',end='') #DEBUG: allows to see tile_num 
+                else:                              #filling whats left with dots
+                    #print('. ',sep='',end='')
+                    print(tile_num,sep='',end='') #DEBUG: allows to see tile_num 
                 
 
                 #printing space between tiles
@@ -120,11 +155,6 @@ class Game():
               end='')
         for i in range(self.table.getSize()[0]): print(alph[i]+'  ', end='')
         
-
-class Piece():
-    def __init__(self):
-        pass
-
 
 
 def main():
